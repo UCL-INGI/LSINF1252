0e1e262fdc152d67516fcea98f727448facbbef0
Mathieu Xhonneux <m.xhonneux@gmail.com>
2018-01-29 11:27:54 +0100

    example: updated test

--- example/student/tests.c
+++ example/student/tests.c
@@ -5,13 +5,18 @@
 #include "CTester/CTester.h"
 
 void test_myfunc_ret() {
-    set_test_metadata("myfunc", _("Brief description of the test"), 1);
+    set_test_metadata("myfunc", _("Test if return = n+1"), 1);
 
     int ret = 0;
 
     SANDBOX_BEGIN;
     ret = myfunc(41);
     SANDBOX_END;
+    push_info_msg(_("Your function didn't fail."))
+
+    if (ret % 2 == 0)
+        push_info_msg(_("You returned a pair number !"));
+
 
     CU_ASSERT_EQUAL(ret,42);
 }
