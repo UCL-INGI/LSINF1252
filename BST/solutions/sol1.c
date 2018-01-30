d4db561098cb7ecf88f391aa572a987de04a8f4f
Maxmawt <max.mawait@gmail.com>
2018-01-30 13:35:49 +0100

    BST solutions

--- BST/solutions/sol1.c
+++ BST/solutions/sol1.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "student_code.h"
 
-/*
+
 int contains_node(node_t* n, int value){
     if(!n) return 0;
     if(n->value == value) return 1;
@@ -14,16 +14,4 @@ int contains_node(node_t* n, int value){
 int contains(bt_t* tree, int value){
     if(!tree) return 0;
     return contains_node(tree->root,value);
-}
-*/
-
-int contains(bt_t* tree, int value){
-    if(!tree) return 0;
-    node_t* runner = tree->root;
-    while (runner) {
-        if(runner->value == value) return 1;
-        if(runner->value < value) runner = runner->right;
-        else runner = runner->left;
-    }
-    return 0;
 }
