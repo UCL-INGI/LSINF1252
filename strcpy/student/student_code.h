9a7c15c3e763e5637b8fef38e03b4d9e227b172b
nrybowski <nicolas.rybowski@student.uclouvain.be>
2018-01-31 11:48:39 +0100

    Adding strcpy

---
+++ strcpy/student/student_code.h
@@ -0,0 +1,4 @@
+#include<string.h>
+#include<stdlib.h>
+
+char *buf_strcpy(const char *src);
---
+++ strcpy/student/tests.c
@@ -0,0 +1,75 @@
+// CTester template
+
+#include <stdlib.h>
+#include "student_code.h"
+#include "CTester/CTester.h"
+
+void test_alloc(const char* src, const char* ret){
+
+// Tests
+//-----------------------------------------------------------------
+
+// check if only one call to malloc
+int ms = stats.malloc.called;
+CU_ASSERT_EQUAL(ms, 1);
+if (ms > 1)
+  push_info_msg(_("You used more than one call to malloc"));
+
+// check if new element is malloced
+int mal = malloced((void*) ret);
+CU_ASSERT_TRUE(mal);
+// if malloced, check the value, else not because it produces buffer overflow due to CUNIT
+if (mal){
+  //printf("%s", ret);
+  CU_ASSERT_STRING_EQUAL(ret, src);
+  if (strncmp(ret, src, strlen(src)) != 0){
+    char msg[80];
+    sprintf(msg, _("The strings are different.\nExpected string: %s. Received value: %s"), src, ret);
+    push_info_msg(msg);
+  }
+}
+else
+  push_info_msg(_("The returned pointer is not malloced"));
+
+}
+
+void test_strcpy_return() {
+  set_test_metadata("buf_strcpy", _("Check the returned pointer in normal case"), 1);
+
+  char *ret = NULL;
+  char *src = "Chaine de char de test un peu courte mais pas trop quand meme";
+
+  monitored.malloc = true;
+
+  SANDBOX_BEGIN;
+  ret = buf_strcpy(src);
+  SANDBOX_END;
+
+test_alloc(src, ret);
+
+}
+
+void test_strcpy_nomem() {
+  set_test_metadata("buf_strcpy", _("Check the behavior of the function when the call to malloc fails"), 1);
+
+  char *ret = NULL;
+  char *src = "Chaine de char de test un peu courte mais pas trop quand meme";
+
+  monitored.malloc = true;
+  failures.malloc = FAIL_ALWAYS;
+
+  SANDBOX_BEGIN;
+  ret = buf_strcpy(src);
+  SANDBOX_END;
+
+  CU_ASSERT_PTR_NULL(ret);
+  if (!ret)
+    push_info_msg(_("The return value of your implementation is wrong"));
+
+}
+
+int main(int argc,char** argv)
+{
+    BAN_FUNCS();
+    RUN(test_strcpy_return, test_strcpy_nomem);
+}
