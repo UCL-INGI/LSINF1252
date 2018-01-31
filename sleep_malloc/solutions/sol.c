e32a6087f5f520b52a5ab7248cb62872ebc6d5ff
INGInious <no-reply@inginious.info.ucl.ac.be>
2018-01-31 12:25:28 +0100

    Automatic synchronization from INGInious

--- sleep_malloc/solutions/sol.c
+++ sleep_malloc/solutions/sol.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include "student_code.h"
 
 void * sleep_malloc(size_t s){
@@ -7,8 +8,9 @@ void * sleep_malloc(size_t s){
     void* p = NULL;
     while(!p && count < 10){
         p = malloc(s);
-        if(!p) sleep(5);
+        if(p) return p;
+        sleep(5);
         count ++;
     }
-    return p;
+    return NULL;
 }
--- sleep_malloc/student/student_code.c.tpl
+++ sleep_malloc/student/student_code.c.tpl
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <unistd.h>
 #include "student_code.h"
 
 void* sleep_malloc(size_t s){
