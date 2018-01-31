ed1ccb9f854c065ae9e1fc706a66c2522eeebb57
INGInious <no-reply@inginious.info.ucl.ac.be>
2018-01-31 14:31:27 +0100

    Automatic synchronization from INGInious

--- s4_file_save_struct/student/tests.c
+++ s4_file_save_struct/student/tests.c
@@ -6,9 +6,10 @@
 
 point_t* gen_struct(int size){
     point_t *tab = malloc(size*sizeof(point_t));
-    if (tab == NULL)
+    if (tab == NULL){
         CU_FAIL("Can not initialize test suite");
         return (point_t*)NULL;
+    }
     for (int i = 0; i < size; i++){
         tab[i].x = i+i;
         tab[i].y = i+i+i;
@@ -21,7 +22,7 @@ point_t* gen_struct(int size){
  * Test with open fail
  */
 void test() {
-    /*set_test_metadata("q1", _("Test write array struct"), 1);
+    set_test_metadata("q1", _("Test write array struct"), 1);
     int size = 6;
     int ret = 0;
     point_t* tab = gen_struct(size);
@@ -41,8 +42,7 @@ void test() {
 
     point_t s;
     for(int i = 0; i < size; i++){
-        int r = read(fd, (void *) &s, sizeof(point_t));
-        if (r == -1){
+        if ((int)read(fd, (void *) &s, sizeof(point_t)) == -1){
             push_info_msg(_("You did not write all content of the array in the file."));
             CU_FAIL();
         }
@@ -57,7 +57,7 @@ void test() {
     if(ret != 0){
         push_info_msg(_("You did not return 0 when everything occurs fine."));
         CU_FAIL(); 
-    }*/
+    }
 }
 
 
