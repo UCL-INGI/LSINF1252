ab3a13e45d875fd6361efd34bb39df88306f51b2
pgonzalezalv <pablo.gonzalez@student.uclouvain.be>
2018-02-01 11:36:53 +0100

    test: testing basic CUnit + Makefile compilation

--- s03_cunit_basics/max_tests.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MAX_TESTS_H_
-#define __MAX_TESTS
-
-#include <stdio.h>
-#include <string.h>
-#include <CUnit/Basic.h>
-
-#endif /* __MAX_TESTS_H_ */
--- s03_cunit_basics/student/make_me.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-/*
- * Prints the string "Make me"  on stdout
- */
-void print_make_me();
