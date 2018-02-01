e2e4a06228d9befbf6743554e36e73b591c64063
VanStratum <arthur.van.stratum@gmail.com>
2018-02-01 16:12:58 +0100

    Add some test

--- palindrome/student/tests.c
+++ palindrome/student/tests.c
@@ -8,17 +8,20 @@ void test_myfunc_ret()
 {
 	set_test_metadata("pal", _("Test the function palindrome"), 1);
 
-	int *ret1 = -2;
-	int *ret2 = -2;
-	int *ret3 = -2;
-	int *ret4 = -2;
-	int *ret5 = -2;
+	int ret1 = -2;
+	int ret2 = -2;
+	int ret3 = -2;
+	int ret4 = -2;
+	int ret5 = -2;
+	int ret6 = -2;
+	int ret7 = -2;
 
-	char *str1 = trap_buffer(44, TRAP_RIGHT, PROT_READ, "Tityre tu patulae recubans sub tegmine fagi");
-	char *str2 = trap_buffer(39, TRAP_RIGHT, PROT_READ, "tu l as trop ecrase cesar ce port salut");
-	char *str3 = trap_buffer(1, TRAP_RIGHT, PROT_READ, "");
-	char *str4 = trap_buffer(23, TRAP_RIGHT, PROT_READ, "azertyuioppptpoiuytreza");
-	char *str5 = trap_buffer(22, TRAP_RIGHT, PROT_READ, "qsdfghjklimmlkjhgfdsq");
+	char *str1 = trap_buffer(44, TRAP_RIGHT, PROT_WRITE, "Tityre tu patulae recubans sub tegmine fagi");
+	char *str2 = trap_buffer(40, TRAP_RIGHT, PROT_WRITE, "tu l as trop ecrase cesar ce port salut");
+	char *str3 = trap_buffer(1, TRAP_RIGHT, PROT_WRITE, "");
+	char *str4 = trap_buffer(25, TRAP_RIGHT, PROT_WRITE, "azertyuioppptpoiuytreza");
+	char *str5 = trap_buffer(22, TRAP_RIGHT, PROT_WRITE, "qsdfghjklmmlkjhgfdsq");
+	char *str6 = trap_buffer(2, TRAP_RIGHT, PROT_WRITE, "e");
 
 	SANDBOX_BEGIN;
 	ret1 = pal(str1);
@@ -27,6 +30,7 @@ void test_myfunc_ret()
 	ret4 = pal(str4);
 	ret5 = pal(str5);
 	ret6 = pal(NULL);
+	ret7 = pal(str6);
 	SANDBOX_END;
 
 	CU_ASSERT_EQUAL(ret1,0);
@@ -35,9 +39,10 @@ void test_myfunc_ret()
 	CU_ASSERT_EQUAL(ret4,0);
 	CU_ASSERT_EQUAL(ret5,1);
 	CU_ASSERT_EQUAL(ret6,-1);
+	CU_ASSERT_EQUAL(ret7,1);
 	if (ret1!=0 || ret4!=0)
 		push_info_msg(_("Your function doesn't work for strings that are not palindromes"));
-	if (ret2!=1 || ret3!=1 || ret5!=1)
+	if (ret2!=1 || ret3!=1 || ret5!=1 || ret7!=1)
 		push_info_msg(_("Your function doesn't work for strings that are palindromes"));
 	if (ret6!=-1)
 		push_info_msg(_("Your function doesn't work for NULL strings"));
