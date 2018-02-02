eba07705d1d670672b76706c7682cf388701a805
VanStratum <arthur.van.stratum@gmail.com>
2018-02-02 13:56:08 +0100

    Add test for bits_spin

--- bits_spin/student/tests.c
+++ bits_spin/student/tests.c
@@ -4,21 +4,25 @@
 #include "student_code.h"
 #include "CTester/CTester.h"
 
-void test_myfunc_ret() {
-    set_test_metadata("myfunc", _("Brief description of the test"), 1);
+void test_myfunc_ret()
+{
+	set_test_metadata("q1", _("Test the functioon cycle_bits"), 1);
 
-    int ret = 0;
+	uint32_t binaryNumber = 0b10110110101010001101111111111111;
+	uint32_t binaryNumberReturned = 0;
 
-    SANDBOX_BEGIN;
-    ret = myfunc(21);
-    SANDBOX_END;
+	SANDBOX_BEGIN;
+	binaryNumberReturned = cycle_bits(binaryNumber,6);
+	SANDBOX_END;
+	CU_ASSERT_EQUAL(binaryNumberReturned,0b10101010001101111111111111101101);
+	if ( binaryNumberReturned!=0b10101010001101111111111111101101)
+		push_info_msg(_("cycle_bits returns the wrong value"));
 
-    CU_ASSERT_EQUAL(ret,0);
 }
 
 int main(int argc,char** argv)
 {
-    BAN_FUNCS();
-    RUN(test_myfunc_ret);
+	BAN_FUNCS();
+	RUN(test_myfunc_ret);
 }
 
