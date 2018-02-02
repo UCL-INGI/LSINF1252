5bb3f90740728d8bc19b21e252539edb8fbec2aa
INGInious <no-reply@inginious.info.ucl.ac.be>
2018-02-02 17:34:26 +0100

    Automatic synchronization from INGInious

--- set_bit/student/tests.c
+++ set_bit/student/tests.c
@@ -10,14 +10,14 @@ void test_myfunc_ret() {
 	uint64_t binaryNumber = 0b10110010;
 	uint64_t binaryNumberReturned;
 	SANDBOX_BEGIN;
-	binaryNumberReturned = set_bit(binaryNumber,2,0);
+	binaryNumberReturned = set_bit(binaryNumber,1,0);
 	SANDBOX_END;
 	CU_ASSERT_EQUAL(binaryNumberReturned,0b10110000);
 	if ( binaryNumberReturned!=0b10110000)
 		push_info_msg(_("set_bit returns the wrong value when setting to 0"));
 	
 	SANDBOX_BEGIN;
-	binaryNumberReturned = set_bit(binaryNumber,7,1);
+	binaryNumberReturned = set_bit(binaryNumber,6,1);
 	SANDBOX_END;
 
 	CU_ASSERT_EQUAL(binaryNumberReturned,0b11110010);
