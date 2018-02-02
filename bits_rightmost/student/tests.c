a72b5a65e0a22af00993b69d985c4b20e5fdf795
INGInious <no-reply@inginious.info.ucl.ac.be>
2018-02-02 13:37:25 +0100

    Automatic synchronization from INGInious

--- bits_rightmost/student/tests.c
+++ bits_rightmost/student/tests.c
@@ -5,14 +5,32 @@ void test_get_4_rightmost_bits(void)
 {
 	set_test_metadata("q1", _("Test the function get_4_rightmost_bits"), 1);
 
-	uint32_t binaryNumber = 0b0111010110110101010001011;
-	uint8_t rightMostBitsReturned;
+	uint32_t binaryNumberA = 0b0111010110110101010001011;
+    uint32_t binaryNumberB = 0b0111010110110101010001110;
+	uint32_t binaryNumberC = 0b0111010110110101010011001;
+
+
+	uint8_t rightMostBitsReturnedA;
+	uint8_t rightMostBitsReturnedB;
+	uint8_t rightMostBitsReturnedC;
+
 
 	SANDBOX_BEGIN;
-	rightMostBitsReturned = get_4_rightmost_bits(binaryNumber);
+	rightMostBitsReturnedA = get_4_rightmost_bits(binaryNumberA);
+	rightMostBitsReturnedB = get_4_rightmost_bits(binaryNumberB);
+	rightMostBitsReturnedC = get_4_rightmost_bits(binaryNumberC);
 	SANDBOX_END;
-	CU_ASSERT_EQUAL(rightMostBitsReturned,0b1011);
-	if ( rightMostBitsReturned!=0b1011)
+    
+	CU_ASSERT_EQUAL(rightMostBitsReturnedA,0b1011);
+	if ( rightMostBitsReturnedA!=0b1011)
+		push_info_msg(_("get_4_rightmost_bits returns the wrong value"));
+    
+	CU_ASSERT_EQUAL(rightMostBitsReturnedB,0b1110);
+	if ( rightMostBitsReturnedB!=0b1110)
+		push_info_msg(_("get_4_rightmost_bits returns the wrong value"));
+    
+	CU_ASSERT_EQUAL(rightMostBitsReturnedC,0b1001);
+	if ( rightMostBitsReturnedC!=0b1001)
 		push_info_msg(_("get_4_rightmost_bits returns the wrong value"));
 }
 
