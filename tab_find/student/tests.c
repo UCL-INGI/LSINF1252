82628317a4eb4852e31bc48c6a525216ce3c2239
INGInious <no-reply@inginious.info.ucl.ac.be>
2018-02-02 15:19:24 +0100

    Automatic synchronization from INGInious

--- tab_find/student/tests.c
+++ tab_find/student/tests.c
@@ -4,24 +4,33 @@
 #include "student_code.h"
 #include "CTester/CTester.h"
 
-void test()
-{
+int tab_find_correct(int tab[], int len, int elem){
+    for(int i = 0; i < len; i++){
+        if(tab[i] == elem)
+            return i;
+    }
+    return -1;
+}
+
+void test(){
     set_test_metadata("tab_find", _("Test function tab find"), 1);    
 
     const int size = 10;
     int array[10] = {1,3,5,7,9,0,8,6,4,2};
     int *tab = trap_buffer(size*sizeof(int), TRAP_RIGHT, PROT_WRITE, array);
-
     int ret = 0;
-    SANDBOX_BEGIN;
-    ret = tab_find(tab, size, 2);
-    SANDBOX_END;
+    
+    for(int i = 0; i < size+10; i++){
+        SANDBOX_BEGIN;
+        ret = tab_find(tab, size, i);
+        SANDBOX_END;
         
-    free_trap(tab,size*sizeof(int));
-    if(ret != 9){
-        push_info_msg(_("Your function does not return the correct index."));
-        CU_FAIL();
+        if(ret != tab_find_correct(tab, size, i)){
+            push_info_msg(_("Your function does not return the correct index."));
+            CU_FAIL();
+        }
     }
+    free_trap(tab,size*sizeof(int));
 }
 
 int main(int argc,char** argv) {
