d986cbfd19e08f79d20c77cfacc2de30aeb0ec1f
VanStratum <arthur.van.stratum@gmail.com>
2018-02-02 11:42:30 +0100

    Copy test for bits_rightmost

--- bits_rightmost/student/tests.c
+++ bits_rightmost/student/tests.c
@@ -1,24 +1,23 @@
-// CTester template
-
-#include <stdlib.h>
 #include "student_code.h"
 #include "CTester/CTester.h"
 
-void test_myfunc_ret() {
-    set_test_metadata("myfunc", _("Brief description of the test"), 1);
+void test_get_4_rightmost_bits(void)
+{
+	set_test_metadata("q1", _("Test the function get_4_rightmost_bits"), 1);
 
-    int ret = 0;
+	uint32_t binaryNumber = 0b0111010110110101010001011;
+	uint8_t rightMostBitsReturned;
 
-    SANDBOX_BEGIN;
-    ret = myfunc(21);
-    SANDBOX_END;
-
-    CU_ASSERT_EQUAL(ret,0);
+	SANDBOX_BEGIN;
+	rightMostBitsReturned = get_4_rightmost_bits(binaryNumber);
+	SANDBOX_END;
+	CU_ASSERT_EQUAL(rightMostBitsReturned,0b1011);
+	if ( rightMostBitsReturned!=0b1011)
+		push_info_msg(_("get_4_rightmost_bits returns the wrong value"));
 }
 
 int main(int argc,char** argv)
 {
-    BAN_FUNCS();
-    RUN(test_myfunc_ret);
+	BAN_FUNCS();
+	RUN(test_get_4_rightmost_bits);
 }
-
