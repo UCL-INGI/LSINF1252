c27426801b9a2576d6966cc8c6e8bed3d2c1d7c4
nrybowski <nicolas.rybowski@student.uclouvain.be>
2018-02-02 16:07:27 +0100

    Adding my_strlen

---
+++ my_strlen/student/tests.c
@@ -0,0 +1,38 @@
+// CTester template
+
+#include <stdlib.h>
+#include "CTester/CTester.h"
+#include "student_code.h"
+
+void test_my_strlen(){
+  set_test_metadata("my_strlen", _("Check the return value of the function"), 2);
+
+  int ret;
+
+  char *s = "26fgb.thjfgjppzaerofa,zprjgfbl";
+
+  SANDBOX_BEGIN;
+  ret = my_strlen(s);
+  SANDBOX_END;
+
+  CU_ASSERT_EQUAL(ret, 30);
+  if (ret != 30)
+    push_info_msg(_("The function return the wrong value"));
+
+  char *s2 = "k26fgb.thjfgjpp";
+
+  SANDBOX_BEGIN;
+  ret = my_strlen(s2);
+  SANDBOX_END;
+
+  CU_ASSERT_EQUAL(ret, 15);
+  if (ret != 15)
+    push_info_msg(_("The function return the wrong value"));
+
+}
+
+int main(int argc,char** argv)
+{
+    BAN_FUNCS();
+    RUN(test_my_strlen);
+}
