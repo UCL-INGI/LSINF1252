76db53e578c2fb79905a08b44f70aeff20f00e48
nrybowski <nicolas.rybowski@student.uclouvain.be>
2018-02-02 18:32:56 +0100

    unfinished linked_struct

---
+++ linked_structs/student/student_code.c.tpl
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include "student_code.h"
+
+struct node *pair_filter(struct node *head) {
+  @@filter@@
+
+}
---
+++ linked_structs/student/student_code.h
@@ -0,0 +1,15 @@
+struct node {
+    struct node *next;
+    int hash;
+    int id;
+    char name[20];
+    char buffer[100];
+    unsigned int timestamp;
+    char acl;
+    short flow;
+    char *parent;
+    void *fifo;
+};
+
+
+struct node *pair_filter(struct node *head);
