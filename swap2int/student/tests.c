432333cabcaa350024ee88433cea1219a291c6cb
INGInious <no-reply@inginious.info.ucl.ac.be>
2018-02-09 22:22:07 +0100

    Automatic synchronization from INGInious

--- swap2int/student/tests.c
+++ swap2int/student/tests.c
@@ -5,7 +5,7 @@
 #include "CTester/CTester.h"
 
 void test_swap() {
-    set_test_metadata("q1", _("Teste que swap échange bien les valeurs"), 1);
+    set_test_metadata("q1", _("Tests whether swap correctly exchanges the contents of the variables"), 1);
     int i = rand();
     int j = rand();
     while (i==j)//to make sure i!=j
@@ -19,7 +19,7 @@ void test_swap() {
     CU_ASSERT_EQUAL(i,j_orig);
     CU_ASSERT_EQUAL(j,i_orig);
     if (i!=j_orig || j!=i_orig)
-	    push_info_msg(_("Les variables ne sont pas bien inversées."));
+	    push_info_msg(_("Your fonction did not correctly exchange the content of the variables."));
 }
 
 int main(int argc,char** argv)
