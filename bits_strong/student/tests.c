5df1ed11072de98312590095b7dba62e47953905
INGInious <no-reply@inginious.info.ucl.ac.be>
2018-02-10 10:10:09 +0100

    Automatic synchronization from INGInious

--- bits_strong/student/tests.c
+++ bits_strong/student/tests.c
@@ -11,9 +11,22 @@ void test_myfunc_ret() {
 	binaryNumberReturned = reset_highestorder_bit(binaryNumber);
 	SANDBOX_END;
 	CU_ASSERT_EQUAL(binaryNumberReturned,0b11011101);
-	if ( binaryNumberReturned!=0b11011101)
-		push_info_msg(_("reset_highestorder_bits returns the wrong value"));
+	if ( binaryNumberReturned!=0b01011101) {
+		push_info_msg(_("reset_highestorder_bits doest not return the correct value"));
+    }
+ 
+    
+    binaryNumber = 0b00000001;
+    SANDBOX_BEGIN;
+	binaryNumberReturned = reset_highestorder_bit(binaryNumber);
+	SANDBOX_END;
+	CU_ASSERT_EQUAL(binaryNumberReturned,0b11011101);
+	if ( binaryNumberReturned!=0b00000000) {
+		push_info_msg(_("reset_highestorder_bits doest not return the correct value"));
+    }
 
+
+    
 }
 
 int main(int argc,char** argv)
