a7bd9964fb9140ab48163a50be5124909b5d4c82
INGInious <no-reply@inginious.info.ucl.ac.be>
2018-02-10 10:22:09 +0100

    Automatic synchronization from INGInious

--- bits_strong/student/tests.c
+++ bits_strong/student/tests.c
@@ -2,6 +2,18 @@
 #include "student_code.h"
 #include "CTester/CTester.h"
 
+uint32_t unset_strong_bit(uint32_t x)
+{
+	if (!x)
+		return 0;
+	for (short i = 1; i<32; i++)
+		if (!(x>>i))
+			return x - (1<<(i-1));
+	return x;
+}
+
+
+
 void test_myfunc_ret() {
 	set_test_metadata("q1", _("Test reset_highestorder_bits"), 1);
 	uint32_t binaryNumber = 0b1011011101;
@@ -10,19 +22,27 @@ void test_myfunc_ret() {
 	SANDBOX_BEGIN;
 	binaryNumberReturned = reset_highestorder_bit(binaryNumber);
 	SANDBOX_END;
-	CU_ASSERT_EQUAL(binaryNumberReturned,0b11011101);
-	if ( binaryNumberReturned!=0b01011101) {
-		push_info_msg(_("reset_highestorder_bits doest not return the correct value"));
+	CU_ASSERT_EQUAL(binaryNumberReturned,unset_strong_bit(binaryNumber));
+	if ( binaryNumberReturned!=unset_strong_bit(binaryNumber)) {
+		push_info_msg(_("reset_highestorder_bits does not return the correct value when its input is 0b0...01011011101"));
     }
- 
-    
     binaryNumber = 0b00000001;
     SANDBOX_BEGIN;
 	binaryNumberReturned = reset_highestorder_bit(binaryNumber);
 	SANDBOX_END;
-	CU_ASSERT_EQUAL(binaryNumberReturned,0b11011101);
-	if ( binaryNumberReturned!=0b00000000) {
-		push_info_msg(_("reset_highestorder_bits doest not return the correct value"));
+CU_ASSERT_EQUAL(binaryNumberReturned,unset_strong_bit(binaryNumber));
+	if ( binaryNumberReturned!=unset_strong_bit(binaryNumber)) {
+		push_info_msg(_("reset_highestorder_bits does not return the correct value when its input is 0b0...0000001"));
+    }
+
+    
+    binaryNumber = 0b00000000;
+    SANDBOX_BEGIN;
+	binaryNumberReturned = reset_highestorder_bit(binaryNumber);
+	SANDBOX_END;
+CU_ASSERT_EQUAL(binaryNumberReturned,unset_strong_bit(binaryNumber));
+	if ( binaryNumberReturned!=unset_strong_bit(binaryNumber)) {
+		push_info_msg(_("reset_highestorder_bits does not return the correct value when its input is 0b0...0000000"));
     }
 
 
