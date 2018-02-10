f4387afdd085316e84af1fd775aa646610061317
INGInious <no-reply@inginious.info.ucl.ac.be>
2018-02-10 10:07:08 +0100

    Automatic synchronization from INGInious

--- bits_strong/student/tests.c
+++ bits_strong/student/tests.c
@@ -3,16 +3,16 @@
 #include "CTester/CTester.h"
 
 void test_myfunc_ret() {
-	set_test_metadata("q1", _("Test unset_strong_bits"), 1);
+	set_test_metadata("q1", _("Test reset_highestorder_bits"), 1);
 	uint32_t binaryNumber = 0b1011011101;
 	uint32_t binaryNumberReturned;
 
 	SANDBOX_BEGIN;
-	binaryNumberReturned = unset_strong_bit(binaryNumber);
+	binaryNumberReturned = reset_highestorder_bit(binaryNumber);
 	SANDBOX_END;
 	CU_ASSERT_EQUAL(binaryNumberReturned,0b11011101);
 	if ( binaryNumberReturned!=0b11011101)
-		push_info_msg(_("unset_strong_bits returns the wrong value"));
+		push_info_msg(_("reset_highestorder_bits returns the wrong value"));
 
 }
 
