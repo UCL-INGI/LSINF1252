d094c9c87df13024cf4a123861ddb291219d9af8
VanStratum <arthur.van.stratum@gmail.com>
2018-02-11 18:52:07 +0100

    Add test for list of one and two element in linked_struct

--- linked_structs/student/tests.c
+++ linked_structs/student/tests.c
@@ -63,6 +63,59 @@ void test_empty_list(){
     push_info_msg(_("Wrong returned value when passing NULL arg"));
 }
 
+void test_one_elem(){
+  set_test_metadata("filter", _("Test the return value of the function when the list is one element long"), 1);
+
+  struct node* ret = (struct node*) 0x42;
+
+  struct node* list = malloc(sizeof(struct node)); 
+  list->next = NULL;
+  list->fifo = NULL;
+  list->parent = '\0';
+
+  SANDBOX_BEGIN;
+  ret = pair_filter(list);
+  SANDBOX_END;
+
+  CU_ASSERT_PTR_NOT_NULL(ret);
+  if (!ret)
+    push_info_msg(_("NULL was returned for a one element list"));
+  CU_ASSERT_PTR_NULL(ret->next);
+  if (memcmp(list, ret, sizeof(struct node))){
+      CU_FAIL("The function produced a wrong list");
+      push_info_msg(_("The function produced a wrong list"));
+  }
+}
+
+void test_two_elem(){
+  set_test_metadata("filter", _("Test the return value of the function when the list is two element long"), 1);
+
+  struct node* ret = (struct node*) 0x42;
+
+  struct node* list = malloc(sizeof(struct node)); 
+  list->fifo = NULL;
+  list->parent = '\0';
+
+  list->next = malloc(sizeof(struct node)); 
+  list->next->next = NULL;
+  list->next->fifo = NULL;
+  list->next->parent = '\0';
+
+  SANDBOX_BEGIN;
+  ret = pair_filter(list);
+  SANDBOX_END;
+
+  CU_ASSERT_PTR_NOT_NULL(ret);
+  if (!ret)
+    push_info_msg(_("NULL was returned for a two element list"));
+  CU_ASSERT_PTR_NULL(ret->next);
+  list->next = NULL;
+  if (memcmp(list, ret, sizeof(struct node))){
+      CU_FAIL("The function produced a wrong list");
+      push_info_msg(_("The function produced a wrong list"));
+  }
+}
+
 void test_pair_filter(){
   set_test_metadata("filter", _("Test if the function returns a valid linked list in normal case"), 1);
   int len = sizeof(struct node);
@@ -105,5 +158,5 @@ void test_pair_filter(){
 int main(int argc,char** argv)
 {
     BAN_FUNCS();
-    RUN(test_pair_filter, test_empty_list);
+    RUN(test_pair_filter, test_empty_list, test_one_elem, test_two_elem);
 }
