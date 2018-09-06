f14aa4c57b5a593a4166937d96d9cc438a0977be
INGInious <no-reply@inginious.info.ucl.ac.be>
2018-09-06 15:25:17 +0200

    Automatic synchronization from INGInious

--- multi-free/student/student_code.h
+++ multi-free/student/student_code.h
@@ -1,1 +1,15 @@
+
+typedef struct university {
+    char* city;
+    int  creation;
+    person_t* rector;
+} university_t;
+
+typedef struct person {
+    char* name;
+    int salary;
+    int age;
+} person_t;
+
+
 int free_all(university_t* u);
