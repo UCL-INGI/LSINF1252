54ca840674c993f94d3789b1004bcba1bcadcb76
INGInious <no-reply@inginious.info.ucl.ac.be>
2018-09-07 14:40:14 +0200

    Automatic synchronization from INGInious

---
+++ Filemap/student/student_code.h
@@ -0,0 +1,13 @@
+typedef struct person {
+    char* name;
+    int salary;
+    int age;
+} person_t;
+
+typedef struct university {
+    char* city;
+    int  creation;
+    person_t* rector;
+} university_t;
+
+int free_all(university_t* u);
