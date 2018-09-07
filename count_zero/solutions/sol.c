7dcaf23be38abb77d7330a6e88df2e1e061ad367
INGInious <no-reply@inginious.info.ucl.ac.be>
2018-09-07 15:22:14 +0200

    Automatic synchronization from INGInious

--- count_zero/solutions/sol.c
+++ count_zero/solutions/sol.c
@@ -5,10 +5,12 @@
 
 int counting_zero(char* str, int length){
     int count = 0;
+    
     int i;
     for(i = 0; i < length; i++){
         if(str[i] = '\0')
             count++;
     }
+    
     return count;
 }
