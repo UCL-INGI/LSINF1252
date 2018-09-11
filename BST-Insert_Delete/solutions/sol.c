3ad5f5e28c9fead649aef40a11d3419704f075b8
INGInious <no-reply@inginious.info.ucl.ac.be>
2018-09-11 18:16:11 +0200

    Automatic synchronization from INGInious

--- BST-Insert_Delete/solutions/sol.c
+++ BST-Insert_Delete/solutions/sol.c
@@ -3,21 +3,6 @@
 #include <unistd.h>
 #include "student_code.h"
 
-int free_all(university_t* u){
-    if(u == NULL){
-        return -1;
-    }
-
-    if(u->rector != NULL){
-        if(u->rector->name != NULL){
-            free(u->rector->name);
-        }
-        free(u->rector);
-    }
-    if(u->city != NULL){
-        free(u->city);
-    }
-    free(u);
-
-    return 0;
-}
+/*
+ * TODO
+ */
