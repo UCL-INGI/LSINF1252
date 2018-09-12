448c735e8cace61974f796f229557c064799eb4d
INGInious <no-reply@inginious.info.ucl.ac.be>
2018-09-12 16:25:13 +0200

    Automatic synchronization from INGInious

--- array_mmap/solutions/sol.c
+++ array_mmap/solutions/sol.c
@@ -1,23 +1,41 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/stat.h>
+#include<sys/mman.h>
+#include<fcntl.h>
+#include<unistd.h>
 #include "student_code.h"
 
-int free_all(university_t* u){
-    if(u == NULL){
+student_t *load_linked_list(char* filename){
+    int fd = open(filename, O_RDWR, S_IRUSR|S_IWUSR);
+    if(fd == -1){
+        return NULL;
+    }
+    
+    struct stat st;
+    fstat(fd, &st);
+    
+    int length = st.st_size;
+    
+    if(length == 0){
+        close(fd);
+        return NULL;
+    }
+    
+    int length_by_struct = length/sizeof(student_t);
+    
+    student_t* map = (student_t*)mmap(NULL, length, PROT_READ, MAP_SHARED, fd, 0);
+    if(map == NULL){
+        close(fd);
+        return NULL;
+    }
+    
+    student_t* root = (student_t*)malloc(sizeof(student_t));
+    if(root == NULL){
+        munmap(map);
+        close(fd);
         return -1;
     }
-
-    if(u->rector != NULL){
-        if(u->rector->name != NULL){
-            free(u->rector->name);
-        }
-        free(u->rector);
-    }
-    if(u->city != NULL){
-        free(u->city);
-    }
-    free(u);
-
-    return 0;
+    
+    root->
 }
--- palindrome/student/tests.c
+++ palindrome/student/tests.c
@@ -53,11 +53,9 @@ void test_myfunc_ret()
 	CU_ASSERT_EQUAL(ret5,1);
 	CU_ASSERT_EQUAL(ret6,-1);
 	CU_ASSERT_EQUAL(ret7,1);
-	if (ret1!=0 ) {
+	if (ret1!=0 || ret2 != 1) {
 		push_info_msg(_("Your function does not work correctly for strings that are not palindromes and contain spaces (remember to first remove spaces from your string) "));
     }
-    if (ret2!=1 )
-		push_info_msg(_("Your function does not work correctly for strings that are palindromes and contain spaces (remember to first remove spaces from your string)"));
     if (ret4!=0 )
 		push_info_msg(_("Your function does not work correctly for strings that are not palindromes"));
 	if (ret2!=1 || ret3!=1 || ret5!=1 || ret7!=1)
