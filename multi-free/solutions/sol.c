de163690f416064045c4b6c511413b2164dc6c27
INGInious <no-reply@inginious.info.ucl.ac.be>
2018-09-13 19:22:28 +0200

    Automatic synchronization from INGInious

--- multi-free/solutions/sol.c
+++ multi-free/solutions/sol.c
@@ -3,6 +3,10 @@
 #include <unistd.h>
 #include "student_code.h"
 
+/*
+ * Frees all the memory associated with u
+ * @pre: u != NULL
+ */
 void free_all(university_t* u){
     if(u->rector != NULL){
         if(u->rector->name != NULL){ 
