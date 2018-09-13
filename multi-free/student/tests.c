c64b51fb53e7e1eec9dbcf32eb2538fb8456c14a
INGInious <no-reply@inginious.info.ucl.ac.be>
2018-09-13 23:14:22 +0200

    Automatic synchronization from INGInious

--- multi-free/student/tests.c
+++ multi-free/student/tests.c
@@ -30,10 +30,10 @@ person_t* init_p(char* name, int age, int salary){
     person_t *p = (person_t*)malloc(sizeof(person_t));
     if(p == NULL)
         return NULL;
-    
+
     p->age = age;
     p->salary = salary;
-    
+
     if(name != NULL){
         char* new_name = (char*)malloc(sizeof(char)*strlen(name));
         if(new_name == NULL){
@@ -41,37 +41,16 @@ person_t* init_p(char* name, int age, int salary){
             return NULL;
         }
         strncpy(new_name, name, strlen(name));
-        
+
         p->name = new_name;
     }
     else{
         p->name = NULL;
     }
-    
+
     return p;
 }
-char* rectname = "Vincent Blondel";
-int age = 53;
-int salary = 4000; //disclaimer : salary is random
-char* city = "Louvain-la-Neuve";
-int creation = 1968; //complex story
 
-person_t* rect = init_p(rectname, age, salary);
-if(!rect){
-    CU_FAIL("Internal problem: can't allocate");
-    return;
-}
-university_t* ucl = init_u(rect, city, creation); //creation: complex story
-if(!ucl){
-    CU_FAIL("Internal problem: can't allocate");
-}
-
-monitored.free = true;
-monitored.malloc = true;
-
-char* city, int creation, char* rectname, int rectsalary, int rectage
-SANDBOX_BEGIN;
-init_all(city, creation, rectname, age, salary)
 
 /*
  * Creates a university_t, with its rector (person_t), its city and
@@ -82,10 +61,10 @@ university_t* init_u(person_t* rector, const char* city, int creation){
     university_t *u = (university_t*)malloc(sizeof(university_t));
     if(u == NULL)
         return NULL;
-    
+
     u->rector = rector;
     u->creation = creation;
-    
+
     if(city != NULL){
         char* new_city = (char*)malloc(sizeof(char)*strlen(city));
         if(new_city == NULL){
@@ -111,12 +90,12 @@ university_t* init_u(person_t* rector, const char* city, int creation){
 university_t* init_new(){
     char* rector_name = "Vincent Blondel";
     char* city_name = "Louvain-la-Neuve";
-    
-    
+
+
     person_t* p = init_p(rector_name, 53, 6000);
     if(p == NULL)
         return NULL;
-    
+
     university_t* u = init_u(p, city_name, 1425);
     if(u == NULL)
         return NULL;
@@ -135,51 +114,58 @@ int free_a(university_t* u){
 }
 
 /*
+ * Checks if two universities have the same values (not checking for NULL cases)
+ */
+int uniEquals(university_t* u1, university_t* u2){
+    return (!strcmp(u1->city,u2->city) && u1->creation==u2->creation && !strcmp((u1->rector)->name,(u2->rector)->name) && (u1->rector)->age==(u2->rector)->age && (u1->rector)->salary==(u2->rector)->salary);
+}
+
+/*
  * Test with a normal case: all the memory should be freed
  */
 void test_success(){
-    
+
     set_test_metadata("free_all", _("Testing in a normal case"), 1);
-    
+
     university_t* u = init_new();
     if(u == NULL)
         return;
-    
+
     university_t* u_c = u;
     char* city_c = u->city;
     person_t* p_c = u->rector;
     char* name_c = u->rector->name;
-    
+
     monitored.free = true;
     monitored.malloc = true;
-    
+
     SANDBOX_BEGIN;
     free_all(u);
     SANDBOX_END;
-    
+
     monitored.malloc = false;
     monitored.free = false;
-    
+
     CU_ASSERT_EQUAL(stats.malloc.called,0);
     if(stats.malloc.called){
         push_info_msg(_("Why are you using malloc ?"));
     }
-    
+
     CU_ASSERT_EQUAL(stats.free.called, 4);
     if(stats.free.called != 4){
         push_info_msg(_("You did not free all the memory"));
     }
-    
+
     int verif_u = malloced(u_c);
     int verif_city = malloced(city_c);
     int verif_rector = malloced(p_c);
     int verif_name = malloced(name_c);
-    
+
     CU_ASSERT_EQUAL(verif_u, false);
     CU_ASSERT_EQUAL(verif_city, false);
     CU_ASSERT_EQUAL(verif_rector, false);
     CU_ASSERT_EQUAL(verif_name, false);
-    
+
     if(verif_u != false){
         push_info_msg(_("You did not free the memory associated with the university"));
     }
@@ -192,12 +178,12 @@ void test_success(){
     if(verif_name != false){
         push_info_msg(_("You did not free the memory associated with the rector's name"));
     }
-    
+
     void *last_ptr = (void*) stats.free.last_params.ptr;
     void *ptr = (void*) u_c;
     CU_ASSERT_EQUAL(ptr, last_ptr);
     if (ptr != last_ptr)
-    push_info_msg(_("The last free you should do is on the struct"));
+        push_info_msg(_("The last free you should do is on the struct"));
 }
 
 /*
@@ -205,52 +191,52 @@ void test_success(){
  */
 void test_rector_null(){
     set_test_metadata("free_all", _("Testing when there is no rector"), 1);
-    
+
     university_t* u = init_u(NULL, "Ottawa", 1848);
     if(u == NULL)
         return;
-    
+
     university_t* u_c = u;
     char* city_c = u->city;
-    
+
     monitored.free = true;
     monitored.malloc = true;
-    
+
     SANDBOX_BEGIN;
     free_all(u);
     SANDBOX_END;
-    
+
     monitored.malloc = false;
     monitored.free = false;
-    
+
     CU_ASSERT_EQUAL(stats.malloc.called,0);
     if(stats.malloc.called){
         push_info_msg(_("Why are you using malloc ?"));
     }
-    
+
     CU_ASSERT_EQUAL(stats.free.called, 2);
     if(stats.free.called != 2){
         push_info_msg(_("You did not free all the memory"));
     }
-    
+
     int verif_u = malloced(u_c);
     int verif_city = malloced(city_c);
-    
+
     CU_ASSERT_EQUAL(verif_u, false);
     CU_ASSERT_EQUAL(verif_city, false);
-    
+
     if(verif_u != false){
         push_info_msg(_("You did not free the memory associated with the university"));
     }
     if(verif_city != false){
         push_info_msg(_("You did not free the memory associated with the city"));
     }
-    
+
     void *last_ptr = (void*) stats.free.last_params.ptr;
     void *ptr = (void*) u_c;
     CU_ASSERT_EQUAL(ptr, last_ptr);
     if (ptr != last_ptr)
-    push_info_msg(_("The last free you should do is on the struct"));
+        push_info_msg(_("The last free you should do is on the struct"));
 }
 
 /*
@@ -258,32 +244,32 @@ void test_rector_null(){
  */
 void test_strings_null(){
     set_test_metadata("free_all", _("Testing when the strings are NULL"), 1);
-    
+
     person_t* p = init_p(NULL, 50, 10000);
     if(p == NULL)
         return;
     university_t* u = init_u(p,NULL,1800);
     if(u == NULL)
         return;
-    
+
     university_t* u_c = u;
     person_t* p_c = u->rector;
-    
+
     monitored.free = true;
     monitored.malloc = true;
-    
+
     SANDBOX_BEGIN;
     free_all(u);
     SANDBOX_END;
-    
+
     monitored.malloc = false;
     monitored.free = false;
-    
+
     CU_ASSERT_EQUAL(stats.malloc.called,0);
     if(stats.malloc.called){
         push_info_msg(_("Why are you using malloc ?"));
     }
-    
+
     CU_ASSERT_EQUAL(stats.free.called,2);
     if(stats.free.called > 2){
         push_info_msg(_("Your code does not work correctly when the strings are NULL"));
@@ -291,25 +277,112 @@ void test_strings_null(){
     if(stats.free.called < 2){
         push_info_msg(_("You did not free all the memory"));
     }
-    
+
     int verif_u = malloced(u_c);
     int verif_rector = malloced(p_c);
-    
+
     CU_ASSERT_EQUAL(verif_u, false);
     CU_ASSERT_EQUAL(verif_rector, false);
-    
+
     if(verif_u != false){
         push_info_msg(_("You did not free the memory associated with the university"));
     }
     if(verif_rector != false){
         push_info_msg(_("You did not free the memory associated with the rector"));
     }
-    
+
     void *last_ptr = (void*) stats.free.last_params.ptr;
     void *ptr = (void*) u_c;
     CU_ASSERT_EQUAL(ptr, last_ptr);
     if (ptr != last_ptr)
-    push_info_msg(_("The last free you should do is on the struct"));
+        push_info_msg(_("The last free you should do is on the struct"));
+}
+
+void test_init_normal_case(){
+    set_test_metadata("init_all", _("Testing the behaviour of the function in a normal case"),1);
+
+    university_t* ucl = init_new();
+    char* city = malloc(sizeof(char)*17);
+    char* name = malloc(sizeof(char)*16);
+
+    if(!ucl || !city || !name){
+        if(ucl) free_a(ucl);
+        if(city) free(city);
+        if(name) free(name);
+        CU_FAIL(_("Internal error while allocating memory"));
+        return;
+    }
+
+    strcpy(city,"Louvain-la-Neuve");
+    strcpy(name,"Vincent Blondel");
+
+    monitored.free = true;
+    monitored.malloc = true;
+    SANDBOX_BEGIN;
+    university_t* ret = init_all(city, creation, rectname, age, salary);
+    SANDBOX_END;
+
+    monitored.malloc = false;
+    monitored.free = false;
+
+
+    CU_ASSERT_EQUAL(stats.free.called, 0);
+    if(stats.free.called)
+        push_info_msg(_("Why did you use free ?"));
+
+    CU_ASSERT_EQUAL(stats.malloc.called, 3);
+    if(stats.malloc.called != 3){
+        push_info_msg(_("You should call malloc 3 times !"));
+        return;
+    }
+
+    if(!ret){
+        CU_FAIL();
+        push_info_msg(_("You returned a null value."));
+        return;
+    }
+
+    if(!(ret->city)){
+        CU_FAIL();
+        push_info_msg(_("You returned a NULL city."));
+        return;
+    }
+
+    if(!(ret->rector)){
+        CU_FAIL();
+        push_info_msg(_("You returned a NULL rector."));
+        return;
+    }
+    if(!(ret->rector)->name){
+        CU_FAIL();
+        push_info_msg(_("You returned a NULL rector name."));
+        return;
+    }
+
+
+    int uniCmp = uniEquals(ret,ucl);
+    CU_ASSERT_EQUAL(uniCmp,true);
+    if(!uniCmp)
+        push_info_msg(_("Your structure isn't the expected one."));
+    else{
+        realloc(city,8);
+        realloc(name,8);
+        if(!city || !name){
+            if(city) free(city);
+            if(name) free(name);
+            free_a(ucl);
+            CU_FAIL(_("Internal error while allocating memory"));
+        }
+        strcpy(city,"changed");
+        strcpy(name,"changed");
+        int cmpChange = uniEquals(ret,ucl);
+        if(!cmpChange){
+            push_info_msg(_("You didn't copy the strings ! You should use strcpy or copy it yourself."));
+            CU_FAIL();
+        }
+    }
+    free_a(ucl);
+
 }
 
 /*
@@ -382,5 +455,5 @@ int compute_graphic(university_t* u){
 int main(int argc,char* argv[])
 {
     BAN_FUNCS(calloc);
-    RUN(test_success, test_rector_null, test_strings_null);
+    RUN(test_success, test_rector_null, test_strings_null, test_init_normal_case);
 }
